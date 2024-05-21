5c444e9ade2efca23a38a8f57447b30a3e91aa32
llabedz-gingco <144914931+llabedz-gingco@users.noreply.github.com>
2024-05-21 10:10:28 +0200

    Update downloadWallpaper.sh

--- downloadWallpaper.sh
+++ downloadWallpaper.sh
@@ -19,8 +19,8 @@
 
 # Define variables
 usebingwallpaper=true # Set to true to have script fetch wallpaper from Bing
-wallpaperurl="https://catlab.blob.core.windows.net/public/win11.jpg?sp=r&st=2023-03-17T20:49:42Z&se=2099-03-18T04:49:42Z&spr=https&sv=2021-12-02&sr=b&sig=ZPpGEOoIoA0lpflLee3G71MT4rtwQ07nAPvrFBOMvUA%3D"
-wallpaperdir="/Users/Shared/"
+wallpaperurl="https://github.com/llabedz-gingco/intune-branding/edit/main/background.jpg"
+wallpaperdir="/System/Library/gingco/"
 wallpaperfile="Wallpaper.jpg"
 log="/var/log/fetchdesktopwallpaper.log"
 
